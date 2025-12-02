4887d380ea181821cfe2c2e28a9410fa79a3d8fe
gonekng <donumm64@gmail.com>
2025-12-02 14:01:07 +0900

    updated

--- app.py
+++ app.py
@@ -261,9 +261,9 @@ def main():
     
     answer_list = df.loc[df['name'] == user_name].values.flatten().tolist()[2:]
     st.session_state.answer_list = answer_list
-    percentage = answer_list.sum / 24
+    percentage = sum(answer_list) / 24
     st.sidebar.write('#### 🧮 퀴즈 진행률')
-    st.sidebar.progress(percentage, text=f'> 총 24문제 중 {answer_list.sum}개 성공')
+    st.sidebar.progress(percentage, text=f'> 총 24문제 중 {sum(answer_list)}개 성공')
     if user_name == None:
         login()
     elif user_name == '관리자':
